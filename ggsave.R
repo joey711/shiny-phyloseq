7421a2d2ee16ccebe7d32f5bb95bc08d97d474f0
Paul J. McMurdie <joey711@gmail.com>
2014-06-01 14:39:42 -0700

    Revision for graphic downloader. Testing needed

--- ggsave.R
+++ ggsave.R
@@ -41,11 +41,8 @@ ggcontent = function(ggplotobj, graphictype="pdf", width=6, height=5, dpi=300L){
   # can play nice together.
   return({
     function(file){
-      ggsave(filename = file, plot = ggplotobj,
-             device = shiny_ggsave_device(width=width, height=height, dpi=dpi, graphictype=graphictype),
-             width=width, height=height, units = "in")
-      # YOU ARE HERE.
-      #ggsave2()
+      ggsave2(filename=file, plot=ggplotobj, device=graphictype, width=width, height=height, dpi=dpi, units="in")
+      #device = shiny_ggsave_device(width=width, height=height, dpi=dpi, graphictype=graphictype),
     }
   })
 }
@@ -54,41 +51,13 @@ ggfilegen = function(prefix, graphictype="pdf"){
     function(){paste0(prefix, gsub("[[:punct:][:space:]]", "_", Sys.time()), ".", graphictype)}
   })
 }
-
-
-
 ggsave2 <- function(filename = "ggplot2save2", plot = last_plot(),
-                   device = "pdf", path = NULL, scale = 1,
+                   device = "pdf", scale = 1,
                    width = par("din")[1], height = par("din")[2], units = c("in", "cm", "mm"),
                    dpi = 300, limitsize = TRUE, ...) {
-  
   if (!inherits(plot, "ggplot")) stop("plot should be a ggplot2 plot")
-  
-  eps <- ps <- function(..., width, height)
-    grDevices::postscript(..., width=width, height=height, onefile=FALSE,
-                          horizontal = FALSE, paper = "special")
-  tex <- function(..., width, height)
-    grDevices::pictex(..., width=width, height=height)
-  pdf <- function(..., version="1.4")
-    grDevices::pdf(..., version=version)
-  svg <- function(...)
-    grDevices::svg(...)
-  wmf <- function(..., width, height)
-    grDevices::win.metafile(..., width=width, height=height)
-  emf <- function(..., width, height)
-    grDevices::win.metafile(..., width=width, height=height)
-  
-  png <- function(..., width, height)
-    grDevices::png(...,  width=width, height=height, res = dpi, units = "in")
-  jpg <- jpeg <- function(..., width, height)
-    grDevices::jpeg(..., width=width, height=height, res = dpi, units = "in")
-  bmp <- function(..., width, height)
-    grDevices::bmp(...,  width=width, height=height, res = dpi, units = "in")
-  tiff <- function(..., width, height)
-    grDevices::tiff(..., width=width, height=height, res = dpi, units = "in")
-  
-  device <- match.fun(device)
-  
+  # Revised `device` interpretation for ggsave2
+  device <- match.fun(paste0("gg", device))
   units <- match.arg(units)
   convert_to_inches <- function(x, units) {
     x <- switch(units,
@@ -97,7 +66,6 @@ ggsave2 <- function(filename = "ggplot2save2", plot = last_plot(),
                 mm = x / 2.54 /10
     )
   }
-  
   convert_from_inches <- function(x, units) {
     x <- switch(units,
                 `in` = x,
@@ -105,7 +73,6 @@ ggsave2 <- function(filename = "ggplot2save2", plot = last_plot(),
                 mm = x * 2.54 * 10
     )
   }
-  
   # dimensions need to be in inches for all graphic devices
   # convert width and height into inches when they are specified
   if (!missing(width)) {
@@ -119,17 +86,11 @@ ggsave2 <- function(filename = "ggplot2save2", plot = last_plot(),
   if (missing(width) || missing(height)) {
     message("Saving ", prettyNum(convert_from_inches(width * scale, units), digits=3), " x ", prettyNum(convert_from_inches(height * scale, units), digits=3), " ", units, " image")
   }
-  
   width <- width * scale
   height <- height * scale
-  
   if (limitsize && (width >= 50 || height >= 50)) {
     stop("Dimensions exceed 50 inches (height and width are specified in inches/cm/mm, not pixels).",
          " If you are sure you want these dimensions, use 'limitsize=FALSE'.")
-  }
-  
-  if (!is.null(path)) {
-    filename <- file.path(path, filename)
   }
   device(file=filename, width=width, height=height, ...)
   on.exit(capture.output(dev.off()))

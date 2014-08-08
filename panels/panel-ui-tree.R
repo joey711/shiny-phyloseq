65a334efac189f04943366881256d64e1e3723ff
Paul J. McMurdie <joey711@gmail.com>
2014-08-07 20:50:04 -0400

    Tree Update. Fix color palette issue for tree.

--- panels/panel-ui-tree.R
+++ panels/panel-ui-tree.R
@@ -1,7 +1,7 @@
 ################################################################################
 # sbp for plot_tree()
 ################################################################################
-sbp_tree = sidebarPanel(actionButton("actionb_tree", "Re-Build Graphic", icon("tree")),
+sbp_tree = sidebarPanel(#actionButton("actionb_tree", "Re-Build Graphic", icon("tree")),
                         selectInput(inputId="method_tree", label="Tree Method", 
                                     choices=list(`No Points`="treeonly", `Dodged Points`="sampledodge")),
                         selectInput(inputId="justify_tree", label="Justify",
@@ -21,7 +21,7 @@ sbp_tree = sidebarPanel(actionButton("actionb_tree", "Re-Build Graphic", icon("t
                         h4('Figure Details'),
                         uiptsz("size_tree"), 
                         uipal("pal_tree"),
-                        uitheme("theme_tree"),
+                        uitheme("theme_tree", default = "blank"),
                         h4('Figure Dimensions'),
                         numericInput("width_tree", "Figure Width (inches)", 8, 1, 100, 1),
                         numericInput("height_tree", "Figure Height (inches)", 8, 1, 100, 1),
